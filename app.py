1416b849d9eccb405180bb88b631a4dc768be891
Grace Nieboer <grace.nieboer@outlook.com>
2023-12-08 17:10:19 -0500

    connect sqlalchemy to postgres database newly created for datenight. use.env file for database URI. Add index route to app with a little hello world message

---
+++ app.py
@@ -0,0 +1,17 @@
+from config import app, api, db
+from flask_restful import Resource
+
+
+
+
+class Index(Resource):
+    def get(self):
+        return f""" hello world """
+    
+
+
+api.add_resource(Index, '/' )
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- config.py
+++ config.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+import os
 
 
 load_dotenv()
 app = Flask(__name__)
 api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+db.init_app(app)
+
+
 
